ce08f2522cf03e0d0ad0130a25817e986484596e
creyes-dev <creyes.dev@gmail.com>
2017-10-04 20:18:18 -0300

    Primera y ultima version

---
+++ Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace Cañones
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblPuntajeJugador1Texto = new System.Windows.Forms.Label();
+            this.lblPuntajeJugador1 = new System.Windows.Forms.Label();
+            this.lblPuntajeJugador2Texto = new System.Windows.Forms.Label();
+            this.lblPuntajeJugador2 = new System.Windows.Forms.Label();
+            this.lblTurnoActual = new System.Windows.Forms.Label();
+            this.lblTurnoJugador = new System.Windows.Forms.Label();
+            this.lblAnguloTexto = new System.Windows.Forms.Label();
+            this.txtAngulo = new System.Windows.Forms.TextBox();
+            this.lblVelocidadTitulo = new System.Windows.Forms.Label();
+            this.txtVelocidadInicial = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnDisparo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblPuntajeJugador1Texto
+            // 
+            this.lblPuntajeJugador1Texto.AutoSize = true;
+            this.lblPuntajeJugador1Texto.Location = new System.Drawing.Point(1081, 13);
+            this.lblPuntajeJugador1Texto.Name = "lblPuntajeJugador1Texto";
+            this.lblPuntajeJugador1Texto.Size = new System.Drawing.Size(96, 13);
+            this.lblPuntajeJugador1Texto.TabIndex = 0;
+            this.lblPuntajeJugador1Texto.Text = "Puntaje Jugador 1:";
+            // 
+            // lblPuntajeJugador1
+            // 
+            this.lblPuntajeJugador1.AutoSize = true;
+            this.lblPuntajeJugador1.Location = new System.Drawing.Point(1183, 14);
+            this.lblPuntajeJugador1.Name = "lblPuntajeJugador1";
+            this.lblPuntajeJugador1.Size = new System.Drawing.Size(0, 13);
+            this.lblPuntajeJugador1.TabIndex = 1;
+            // 
+            // lblPuntajeJugador2Texto
+            // 
+            this.lblPuntajeJugador2Texto.AutoSize = true;
+            this.lblPuntajeJugador2Texto.Location = new System.Drawing.Point(1209, 13);
+            this.lblPuntajeJugador2Texto.Name = "lblPuntajeJugador2Texto";
+            this.lblPuntajeJugador2Texto.Size = new System.Drawing.Size(96, 13);
+            this.lblPuntajeJugador2Texto.TabIndex = 2;
+            this.lblPuntajeJugador2Texto.Text = "Puntaje Jugador 2:";
+            // 
+            // lblPuntajeJugador2
+            // 
+            this.lblPuntajeJugador2.AutoSize = true;
+            this.lblPuntajeJugador2.Location = new System.Drawing.Point(1311, 14);
+            this.lblPuntajeJugador2.Name = "lblPuntajeJugador2";
+            this.lblPuntajeJugador2.Size = new System.Drawing.Size(0, 13);
+            this.lblPuntajeJugador2.TabIndex = 3;
+            // 
+            // lblTurnoActual
+            // 
+            this.lblTurnoActual.AutoSize = true;
+            this.lblTurnoActual.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTurnoActual.Location = new System.Drawing.Point(12, 13);
+            this.lblTurnoActual.Name = "lblTurnoActual";
+            this.lblTurnoActual.Size = new System.Drawing.Size(70, 13);
+            this.lblTurnoActual.TabIndex = 4;
+            this.lblTurnoActual.Text = "Turno actual:";
+            // 
+            // lblTurnoJugador
+            // 
+            this.lblTurnoJugador.AutoSize = true;
+            this.lblTurnoJugador.Location = new System.Drawing.Point(92, 13);
+            this.lblTurnoJugador.Name = "lblTurnoJugador";
+            this.lblTurnoJugador.Size = new System.Drawing.Size(0, 13);
+            this.lblTurnoJugador.TabIndex = 5;
+            // 
+            // lblAnguloTexto
+            // 
+            this.lblAnguloTexto.AutoSize = true;
+            this.lblAnguloTexto.Location = new System.Drawing.Point(145, 13);
+            this.lblAnguloTexto.Name = "lblAnguloTexto";
+            this.lblAnguloTexto.Size = new System.Drawing.Size(43, 13);
+            this.lblAnguloTexto.TabIndex = 6;
+            this.lblAnguloTexto.Text = "Ángulo:";
+            // 
+            // txtAngulo
+            // 
+            this.txtAngulo.Location = new System.Drawing.Point(194, 6);
+            this.txtAngulo.Name = "txtAngulo";
+            this.txtAngulo.Size = new System.Drawing.Size(35, 20);
+            this.txtAngulo.TabIndex = 7;
+            this.txtAngulo.Text = "45";
+            // 
+            // lblVelocidadTitulo
+            // 
+            this.lblVelocidadTitulo.AutoSize = true;
+            this.lblVelocidadTitulo.Location = new System.Drawing.Point(245, 13);
+            this.lblVelocidadTitulo.Name = "lblVelocidadTitulo";
+            this.lblVelocidadTitulo.Size = new System.Drawing.Size(57, 13);
+            this.lblVelocidadTitulo.TabIndex = 8;
+            this.lblVelocidadTitulo.Text = "Velocidad:";
+            // 
+            // txtVelocidadInicial
+            // 
+            this.txtVelocidadInicial.Location = new System.Drawing.Point(305, 6);
+            this.txtVelocidadInicial.Name = "txtVelocidadInicial";
+            this.txtVelocidadInicial.Size = new System.Drawing.Size(39, 20);
+            this.txtVelocidadInicial.TabIndex = 9;
+            this.txtVelocidadInicial.Text = "200";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(350, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "m/s";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(228, 6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(11, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "º";
+            // 
+            // btnDisparo
+            // 
+            this.btnDisparo.Location = new System.Drawing.Point(390, 4);
+            this.btnDisparo.Name = "btnDisparo";
+            this.btnDisparo.Size = new System.Drawing.Size(75, 23);
+            this.btnDisparo.TabIndex = 12;
+            this.btnDisparo.Text = "Disparo";
+            this.btnDisparo.UseVisualStyleBackColor = true;
+            this.btnDisparo.Click += new System.EventHandler(this.btnDisparo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1384, 782);
+            this.Controls.Add(this.btnDisparo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtVelocidadInicial);
+            this.Controls.Add(this.lblVelocidadTitulo);
+            this.Controls.Add(this.txtAngulo);
+            this.Controls.Add(this.lblAnguloTexto);
+            this.Controls.Add(this.lblTurnoJugador);
+            this.Controls.Add(this.lblTurnoActual);
+            this.Controls.Add(this.lblPuntajeJugador2);
+            this.Controls.Add(this.lblPuntajeJugador2Texto);
+            this.Controls.Add(this.lblPuntajeJugador1);
+            this.Controls.Add(this.lblPuntajeJugador1Texto);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblPuntajeJugador1Texto;
+        private System.Windows.Forms.Label lblPuntajeJugador1;
+        private System.Windows.Forms.Label lblPuntajeJugador2Texto;
+        private System.Windows.Forms.Label lblPuntajeJugador2;
+        private System.Windows.Forms.Label lblTurnoActual;
+        private System.Windows.Forms.Label lblTurnoJugador;
+        private System.Windows.Forms.Label lblAnguloTexto;
+        private System.Windows.Forms.TextBox txtAngulo;
+        private System.Windows.Forms.Label lblVelocidadTitulo;
+        private System.Windows.Forms.TextBox txtVelocidadInicial;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnDisparo;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,396 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
+
+namespace Cañones
+{
+    public partial class Form1 : Form
+    {
+        // Proporciones y posiciones
+        const double anchoCañon = 73;
+        const double altoCañon = 40;
+        const int posicionXJugador1 = 10;
+        const int posicionXJugador2 = 1280;
+        const int radioBola = 20;
+
+        // Variables del juego
+        bool turnoJugador2;
+        short puntosJugador1;
+        short puntosJugador2;
+
+        int posicionYJugador1;
+        int posicionYJugador2;
+        int posicionConvencionalYJugador1;
+        int posicionConvencionalYJugador2;
+        
+        int posicionInicialXBola;
+        int posicionInicialYBola;
+        int posicionXBola;
+        int posicionYBola;
+
+        bool bolaDisparada;
+        long decimasSegundoDesdeDisparo;
+        double radianesAngulo;
+        double componenteVectorVelocidadInicialX;
+        double componenteVectorVelocidadInicialY;
+
+        // Variables utilizadas para la detección de colisiones
+        int limiteSuperiorCañon1;
+        int limiteInferiorCañon1;
+        int limiteSuperiorCañon2;
+        int limiteInferiorCañon2;
+
+        int limiteIzquierdoCañon1;
+        int limiteDerechoCañon1;
+        int limiteIzquierdoCañon2;
+        int limiteDerechoCañon2;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Obtiene la coordenada convencional en el eje Y y la convierte a 
+        // una coordenada que corresponde con el sistema de coordenadas
+        // del winform
+        private int CoordenadaVerticalWinForm(int coordVerticalConvencional)
+        {
+            int coordenadaVerticalWinForm = this.Height - coordVerticalConvencional;
+            return coordenadaVerticalWinForm;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            NuevoJuego();
+        }
+
+        // 1. NUEVO JUEGO
+        private void NuevoJuego()
+        {
+            // Cada vez que se inicia un nuevo juego se reinician los
+            // puntajes en cero
+            puntosJugador1 = 0;
+            puntosJugador2 = 0;
+
+            NuevoTurno();
+            this.timer1.Enabled = true;
+        }
+
+        // 2. NUEVO TURNO
+        private void NuevoTurno()
+        {
+            // Mostrar los puntajes
+            this.lblPuntajeJugador1.Text = puntosJugador1.ToString();
+            this.lblPuntajeJugador2.Text = puntosJugador2.ToString();
+
+            // Mostrar el turno del jugador
+            this.lblTurnoJugador.Text = (turnoJugador2) ? "Jugador 2" : "Jugador 1";
+            
+            // 3. VERIFICAR PUNTAJES
+            // Si es el turno del jugador 1 verificar si algún jugador ya ha ganado
+            if (!turnoJugador2)
+            {
+                // Si ambos tienen 3 o mas puntos algunos de los dos ya pudo haber ganado
+                if (puntosJugador1 >= 3 || puntosJugador2 >= 3)
+                {
+                    // Si ambos poseen distinto puntaje es porque no hay empate y uno de los
+                    // dos ha ganado
+                    if (puntosJugador1 != puntosJugador2)
+                    {
+                        FinDelJuego();
+                    }
+                }
+            }
+
+            // Posicionar los cañones en un punto al azar
+            Random r = new Random();
+            posicionYJugador1 = r.Next(200, 700);
+            posicionYJugador2 = r.Next(200, 700);
+
+            posicionConvencionalYJugador1 = posicionYJugador1;
+            posicionConvencionalYJugador2 = posicionYJugador2;
+
+            posicionYJugador1 = CoordenadaVerticalWinForm(posicionYJugador1);
+            posicionYJugador2 = CoordenadaVerticalWinForm(posicionYJugador2);
+
+            // Registrar en variables valores utilizados para la detección de colisiones
+            limiteSuperiorCañon1 = this.posicionYJugador1;
+            limiteInferiorCañon1 = this.posicionYJugador1 + Convert.ToInt32(altoCañon);
+            limiteSuperiorCañon2 = this.posicionYJugador2;
+            limiteInferiorCañon2 = this.posicionYJugador2 + Convert.ToInt32(altoCañon);
+            limiteIzquierdoCañon1 = posicionXJugador1;
+            limiteDerechoCañon1 = posicionXJugador1 + Convert.ToInt32(Math.Round(anchoCañon,0));
+            limiteIzquierdoCañon2 = posicionXJugador2;
+            limiteDerechoCañon2 = posicionXJugador2 + Convert.ToInt32(Math.Round(anchoCañon, 0));
+
+            // Informar de quien es el nuevo turno
+            string descrTurno = "turno del " + this.lblTurnoJugador.Text;
+            MessageBox.Show(descrTurno);
+
+            // Habilitar el boton de tiro
+            btnDisparo.Enabled = true;
+        }
+
+        private void FinTurno()
+        {
+            turnoJugador2 = !turnoJugador2;
+            bolaDisparada = false;
+        }
+
+        // 4. DISPARAR CAÑON
+        private void btnDisparo_Click(object sender, EventArgs e)
+        {
+            // Posicionar la bola en el cañón
+            // Definir las posiciones utilizando el sistema de coordenadas
+            // convencional. Al desplazarse bola será dibujada
+            // utilizando el sistema de coordenadas del winform 
+            if (turnoJugador2)
+            {
+                posicionInicialXBola = posicionXJugador2 - 5;
+                posicionInicialYBola = posicionConvencionalYJugador2;
+            }
+            else
+            {
+                posicionInicialXBola = posicionXJugador1 + Convert.ToInt32(anchoCañon);
+                posicionInicialYBola = posicionConvencionalYJugador1;
+            }
+
+            posicionXBola = posicionInicialXBola;
+            posicionYBola = posicionInicialYBola;
+
+            // Calcular los componentes del vector velocidad inicial 
+            // que son necesarios para calcular la posición del 
+            // proyectil en determinado momento
+            CalcularComponentesVectorVelocidad();
+
+            decimasSegundoDesdeDisparo = 0;
+            bolaDisparada = true;
+
+            this.btnDisparo.Enabled = false;
+        }
+
+        // Cada 1 decima de segundo dibujar los elementos del juego
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Refresh();
+
+            // 5. MOVER LA BOLA
+            DibujarBola();
+            DibujarJugadores();
+            // 6. DETECTAR COLISIONES
+            ControlarColisionesBola();
+        }
+
+        private void DibujarBola()
+        {
+            if (bolaDisparada)
+            {
+                Brush colorBola = Brushes.Blue;
+                int direccionHorizontalBola = 1;
+
+                if (turnoJugador2)
+                {
+                    colorBola = Brushes.Red;
+                    direccionHorizontalBola = -1;
+                }
+
+                // han pasado 100ms (1 decima de segundo)
+                // porque esta funcion es llamada cada 100ms
+                decimasSegundoDesdeDisparo += 1;
+
+                // calcular la nueva coordenada de x
+                //X =X_0 + V_0x(t) 
+                double posicionX = Math.Round(posicionInicialXBola + direccionHorizontalBola * (componenteVectorVelocidadInicialX * (decimasSegundoDesdeDisparo * 0.1)), 0);
+                posicionXBola = Convert.ToInt32(posicionX);
+
+                // calcular la nueva coordenada de y
+                // Y = Y_0 + V_0y(t-t_0)-1/2g(t-t_0)^2
+                double decimasSegundoAlCuadrado = decimasSegundoDesdeDisparo * 0.1;
+                double posicionY = Math.Round(((posicionInicialYBola + componenteVectorVelocidadInicialY * (decimasSegundoDesdeDisparo * 0.1)) - (0.5 * 9.8 * Math.Pow(decimasSegundoDesdeDisparo * 0.1, 2))), 0);
+                posicionYBola = Convert.ToInt32(Math.Round(posicionY, 0));
+                posicionYBola = CoordenadaVerticalWinForm(posicionYBola);
+
+                //Dibujar la bola
+                Graphics graficos = this.CreateGraphics();
+                graficos.FillEllipse(colorBola, posicionXBola, posicionYBola, radioBola, radioBola);
+            }
+        }
+
+        private void DibujarJugadores()
+        {
+            Graphics graficos = this.CreateGraphics();
+
+            string directorioEjecutable = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string dirImagenJugador = directorioEjecutable + "\\cannon1.png";
+
+            Bitmap imagen = new Bitmap(dirImagenJugador);
+            graficos.DrawImageUnscaled(imagen, posicionXJugador1, posicionYJugador1);
+
+            dirImagenJugador = directorioEjecutable + "\\cannon2.png";
+            imagen = new Bitmap(dirImagenJugador);
+            graficos.DrawImageUnscaled(imagen, posicionXJugador2, posicionYJugador2);
+        }
+
+        private void ControlarColisionesBola()
+        {
+            if (bolaDisparada)
+            {
+                // 7. DETECTAR SI LA BOLA TRASPASA LOS LIMITES DEL PLANO
+                // Verificar si la bola pasa los límites de la ventana 
+                if (BolaTraspasaLimitesVentana())
+                {
+                    FinTurno();
+                    NuevoTurno();
+                }
+                else
+                {
+                    if (BolaImpactaCañon())
+                    {
+                        if (turnoJugador2)
+                        {
+                            puntosJugador2++;
+                        }
+                        else
+                        {
+                            puntosJugador1++;
+                        }
+
+                        FinTurno();
+                        MessageBox.Show("La bola ha impactado al cañón, el jugador gana un punto");
+                        NuevoTurno();
+                    }
+                }
+
+            }
+        }
+
+        private void FinDelJuego()
+        {
+            timer1.Enabled = false;
+
+            // Definir ganador
+            string jugadorGanador = "Jugador 1";
+            if (puntosJugador2 > puntosJugador1)
+            {
+                MessageBox.Show("El ganador es " + jugadorGanador);
+            }
+
+            // Preguntar si desea seguir jugando
+            if (MessageBox.Show("¿Desea jugar nuevamente?", "Nuevo juego", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                NuevoJuego();
+            }
+            else
+            {
+                Application.Exit();
+            }
+        }
+        
+        private bool BolaTraspasaLimitesVentana()
+        {
+            bool bolaTraspasaLimitesVentana = false;
+
+            // La bola cae traspasando el límite inferior de la ventana
+            if (posicionYBola > this.Height)
+            {
+                bolaTraspasaLimitesVentana = true;
+            }
+            else
+            {
+                // La bola traspasa el límite derecho de la ventana
+                if (posicionXBola > this.Width)
+                {
+                    bolaTraspasaLimitesVentana = true;
+                }
+                else
+                {
+                    // La bola traspasa el límite izquierdo de la ventana
+                    if (posicionXBola - radioBola < 0)
+                    {
+                        bolaTraspasaLimitesVentana = true;
+                    }
+                }
+            }
+            return bolaTraspasaLimitesVentana;
+        }
+
+        private bool BolaImpactaCañon()
+        {
+            bool bolaImpactaCañon = false;
+
+            // int limiteSuperiorBola = CoordenadaVerticalWinForm(posicionYBola);
+            int limiteSuperiorBola = posicionYBola;
+            int limiteInferiorBola = limiteSuperiorBola + (radioBola);
+            int limiteIzquierdoBola = posicionXBola;
+            int limiteDerechoBola = posicionXBola + (radioBola);
+
+            // Verificar si los límites superior e inferior
+            // de la bola se encuentran posicionados dentro
+            // del rango de valores de los límites superior e 
+            // inferior del cañón
+
+            int limiteSuperiorCañon = limiteSuperiorCañon2;
+            int limiteInferiorCañon = limiteInferiorCañon2 + 12;
+            int limiteIzquierdoCañon = limiteIzquierdoCañon2;
+            int limiteDerechoCañon = limiteDerechoCañon2;
+
+            if (turnoJugador2)
+            {
+                limiteSuperiorCañon = limiteSuperiorCañon1;
+                limiteInferiorCañon = limiteInferiorCañon1 + 12;
+                limiteIzquierdoCañon = limiteIzquierdoCañon1;
+                limiteDerechoCañon = limiteDerechoCañon1;
+            }
+
+            // Verificar si la bola se encuentra en los límites superior y superior del cañón  
+            if (limiteSuperiorCañon <= limiteInferiorBola && limiteInferiorCañon >= limiteSuperiorBola)
+            {
+                if (limiteIzquierdoBola <= limiteDerechoCañon && limiteDerechoBola >= limiteIzquierdoCañon)
+                {
+                    bolaImpactaCañon = true;
+
+                }
+            }
+
+            Graphics graficos = this.CreateGraphics();
+
+            graficos.FillRectangle(Brushes.DarkOliveGreen, 1, limiteSuperiorCañon, 1400, 1);
+            graficos.FillRectangle(Brushes.DarkOliveGreen, 1, limiteInferiorCañon, 1400, 1);
+
+            graficos.FillRectangle(Brushes.DarkOliveGreen, limiteIzquierdoCañon, 1, 1, 800);
+            graficos.FillRectangle(Brushes.DarkOliveGreen, limiteDerechoCañon, 1, 1, 800);
+
+            graficos.FillRectangle(Brushes.DarkRed, 1, limiteSuperiorBola, 1400, 1);
+            graficos.FillRectangle(Brushes.DarkRed, 1, limiteInferiorBola, 1400, 1);
+
+            graficos.FillRectangle(Brushes.DarkRed, limiteIzquierdoBola, 1, 1, 800);
+            graficos.FillRectangle(Brushes.DarkRed, limiteDerechoBola, 1, 1, 800);
+            
+            return bolaImpactaCañon;
+        }
+        
+        private void CalcularComponentesVectorVelocidad()
+        {   
+            double velocidadInicial = Convert.ToDouble(this.txtVelocidadInicial.Text);
+            double angulo = Convert.ToDouble(this.txtAngulo.Text);
+
+            // Convierto los angulos a radianes 
+            radianesAngulo = angulo * (Math.PI / 180);
+
+            // V_0x = V_0*Cos(alpha)
+            componenteVectorVelocidadInicialX = velocidadInicial * Math.Cos(radianesAngulo);
+
+            // V_0y = V_0*sin(alpha)
+            componenteVectorVelocidadInicialY = velocidadInicial * Math.Sin(radianesAngulo);
+        }
+
+    }
+}
